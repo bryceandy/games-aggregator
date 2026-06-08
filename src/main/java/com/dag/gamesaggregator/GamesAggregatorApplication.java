d3147da05a42596bd5938329e50c9f927102369f
Bryce Kidayi <b.kidayi@zunocarbon.com>
2026-06-08 03:37:23 +0300

    Spotless check and fix

--- src/main/java/com/dag/gamesaggregator/GamesAggregatorApplication.java
+++ src/main/java/com/dag/gamesaggregator/GamesAggregatorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GamesAggregatorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GamesAggregatorApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(GamesAggregatorApplication.class, args);
+  }
 }
--- src/test/java/com/dag/gamesaggregator/GamesAggregatorApplicationTests.java
+++ src/test/java/com/dag/gamesaggregator/GamesAggregatorApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class GamesAggregatorApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
